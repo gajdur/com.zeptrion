8d1ec4510925de03d28e4efb9e7d13691cb5880f
gajdur <iot@gjorgjiev.ch>
2016-09-22 22:19:11 +0200

    Update driver.js

--- drivers/zeptrion_air_2k/driver.js
+++ drivers/zeptrion_air_2k/driver.js
@@ -53,43 +53,6 @@ module.exports.pair = function (socket) {
 	});
 };
 
-// these are the methods that respond to get/set calls from Homey
-// for example when a user pressed a button
-module.exports.capabilities = {};
-module.exports.capabilities.onoff = {};
-module.exports.capabilities.onoff.get = function( device_data, callback ) {
-    var device = getDeviceByData( device_data );
-    if( device instanceof Error ) return callback( device );
-    return callback( null, device.state.onoff );
-}
-module.exports.capabilities.onoff.set = function( device_data, onoff, callback ) {
-    var device = getDeviceByData( device_data );
-    if( device instanceof Error ) return callback( device );
-    device.state.onoff = onoff;
-    // here you would use a wireless technology to actually turn the device on or off
-    // also emit the new value to realtime
-    // this produced Insights logs and triggers Flows
-    self.realtime( device_data, 'onoff', device.state.onoff)
-    return callback( null, device.state.onoff );
-}
-
-// a helper method to get a device from the devices list by it's device_data object
-function getDeviceByData( device_data ) {
-    var device = devices[ device_data.id ];
-    if( typeof device === 'undefined' ) {
-        return new Error("invalid_device");
-    } else {
-        return device;
-    }
-}
-
-// a helper method to add a device to the devices list
-function initDevice( device_data ) {
-    devices[ device_data.id ] = {};
-    devices[ device_data.id ].state = { onoff: true };
-    devices[ device_data.id ].data = device_data;
-}
-
 // flow action handlers
 Homey.manager('flow').on('action.zeptrion_cmd', function (callback, args) {
 	var device = args.device;
